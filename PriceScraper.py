805f4531354e41085cbcb806e7e5f55bca50b51e
Bert Deneef <bert.deneef@unilin.com>
2023-07-06 09:25:27 +0200

    Included full scraper

--- PriceScraper.py
+++ PriceScraper.py
@@ -24,14 +24,22 @@ else:
     # Get the current working directory
     base_path = os.getcwd()
 url_path = os.path.join(base_path, 'urls.txt')
-output_path = os.path.join(base_path, 'output.csv')
+full_url_path = os.path.join(base_path, 'full_urls.txt')
+output_path = os.path.join(base_path, 'individual_scrapes.csv')
+full_output_path = os.path.join(base_path, 'full_scrapes.csv')
 
 # Define variables
 
 names = []
+full_names = []
 urls = []
+full_urls = []
 prices = []
+full_prices = []
 units = []
+full_units = []
+result_names = []
+products = []
 
 # Read the urls from the file and store them in a list
 
@@ -40,7 +48,14 @@ with open(url_path, 'r') as f:
         data = line.strip()
         names.append(data.split(';')[0])
         urls.append(data.split(';')[1])
-   
+
+# Read the full urls from the file and store them in a list
+
+with open(full_url_path, 'r') as f:
+    for line in f:
+        data = line.strip()
+        full_names.append(data.split(';')[0])
+        full_urls.append(data.split(';')[1])
 
 # start by defining the options 
 options = webdriver.ChromeOptions() 
@@ -101,6 +116,56 @@ for url in urls:
     prices.append(price)
     units.append(unit)
 
+for url in full_urls:
+    driver.get(url) 
+    time.sleep(2)
+    # Click on the "Accept" button which has the following html: 
+    try:
+        driver.find_element(By.CSS_SELECTOR, "#CybotCookiebotDialogBodyButtonAccept").click()
+        time.sleep(2)
+    except:
+        pass
+
+    try:
+        # On the page, there is a dropdown menu with class "sort-number". This needs to be clicked and the option where the value contains the string "PageSize_96" needs to be clicked. This will show 96 products per page instead of 24 products per page.
+        driver.find_element(By.CSS_SELECTOR, ".sort-number").click()
+        time.sleep(2)
+        driver.find_element(By.CSS_SELECTOR, "[value*='PageSize_96']").click()
+        time.sleep(10)
+    except:
+        pass
+    
+    soup = bs4.BeautifulSoup(driver.page_source, 'html.parser')
+
+    # Find all the divs with class "product-item" and store them in a list
+    product_divs = soup.find_all('div', {'class': 'product-item'})
+
+    # Get the name corresponding to the url
+    name = full_names[full_urls.index(url)]
+
+    for product_div in product_divs:
+        # find the label with class product-description-label
+        product_description_label = product_div.find('label', {'class': 'product-description-label'})
+
+        # find the div with class actual-price-price
+        actual_price_price = product_div.find('div', {'class': 'actual-price-price'})
+
+        # find the label with class current-price
+        current_price = actual_price_price.find('label', {'class': 'current-price'})
+
+        # find the label with class unit-current
+        unit_current = actual_price_price.find('label', {'class': 'unit-current'})
+
+        # Extract the text inside the product_description_label, current_price and unit_current, and remove the whitespace
+        product = product_description_label.text.strip()
+        price = current_price.text.strip()
+        unit = unit_current.text.strip()
+        price = ''.join([i for i in price if i.isdigit() or i == ','])
+
+        result_names.append(name)
+        products.append(product)
+        full_prices.append(price)
+        full_units.append(unit)
 
 # Convert names, prices, units to a csv file, also add the current date and hour in 2 columns
 # Set the date and hour as the first 2 columns
@@ -111,6 +176,7 @@ hour = time.strftime('%H:%M:%S')
 # Create a dataframe with the data
 
 df = pd.DataFrame({'Date': date, 'Hour': hour, 'Name': names, 'Price': prices, 'Unit': units})
+df_full = pd.DataFrame({'Date': date, 'Hour': hour, 'Name': result_names, 'Product': products, 'Price': full_prices, 'Unit': full_units})
 
 # Save the dataframe to a csv file
 
@@ -118,5 +184,10 @@ if os.path.exists(output_path):
     df.to_csv(output_path, mode='a', header=False, index=False, sep=';')
 else:
     df.to_csv(output_path, index=False, sep=';')
+
+if os.path.exists(full_output_path):
+    df_full.to_csv(full_output_path, mode='a', header=False, index=False, sep=';')
+else:
+    df_full.to_csv(full_output_path, index=False, sep=';')
 
 driver.quit()
